236ebcabdd5bab89b6fe97d5f07c6862c239d122
Ryan Larson <ryan-p-larson@uiowa.edu>
2017-06-12 21:11:38 -0500

    Moved chart creation into functions and made it reusable for different keys

--- src/immigrouped.js
+++ src/immigrouped.js
@@ -24,7 +24,7 @@ var reuseableBar = function(_myData) {
       .rangeRound([0, width]).paddingInner(0.1);
   var x_1 = d3.scaleBand().padding(0.1);
   var y = d3.scaleLinear().rangeRound([height, 0]);
-  var color = d3.scaleOrdinal(['#addd8e', '#238443', '#fec44f']);
+  var color = d3.scaleOrdinal(['#addd8e', '#238443', '#fec44f', '#d95f0e']);
 
   var parse_date = d3.timeParse('%Y-%m-%d');
 
@@ -48,11 +48,34 @@ var reuseableBar = function(_myData) {
     });
   }
 
+  chartAPI.keys = function(value) {
+    if (!arguments.length) return keys;
+    keys = value;
+
+    var nested = get_nest(data, 'date');
+    set_scale(nested);
+    draw_bars(nested);
+    draw_axes();
+    draw_legend();
+    //return chartAPI;
+  };
 
   ////////////////////////////////////
   // 3.0 add private functions here //
   ////////////////////////////////////
-
+  function get_nest(data, key) {
+    var nested = d3.nest()
+        .key(function(d) { return d[key]; })
+        .rollup(function(v) {
+          var vals = {};
+          keys.forEach(function(k) {
+            vals[k] = d3.sum(v, function(v){ return v[k]; });
+          });
+          return vals;
+        })
+        .entries(data);
+    return nested;
+  }
 
   ////////////////////////////////////////////////////
   // 4.0 add visualization specific processing here //
@@ -78,96 +101,108 @@ var reuseableBar = function(_myData) {
         .attr('class', 'barChart')
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      var nested = d3.nest()
-          .key(function(d) { return d.date; })
-          .rollup(function(v) { return {
-            immigrant: d3.sum(v, function(v) { return v.immigrant; }),
-            immigration: d3.sum(v, function(v) { return v.immigration; }),
-            nonimmigrant: d3.sum(v, function(v) { return v.nonimmigrant; })
-            };
-          })
-          .entries(data);
-      console.log(nested);
+      // Group Data
+      var nested = get_nest(data, 'date');
 
       // SCALE SETTING
-      y.domain([0, d3.max(nested.map(function(d) { return d.value; }),
-        function(d) {
-          return d3.max(keys, function(key) { return d[key]; });
-        })
-      ]);
+      set_scale(nested);
 
-      x_0.domain(date_ticks);
-      x_1.domain(keys).rangeRound([0, x_0.bandwidth()]);
-      color.domain(keys);
-
-      // DATA TO CHART CALC
-      var week = g.append('g').attr('class', 'weeks').selectAll('.week')
-          .data(nested)
-            .enter().append('g')
-          .attr('class', 'week')
-          .attr('transform', function(d) {
-            return 'translate('+ x_0(d.key) + ',0)'; });
-      // RECTS
-      week.selectAll('rect')
-        .data(function(d) { return keys.map(function(key) { return {key: key, value: d.value[key]}; }); })
-          .enter().append('rect')
-        .attr('x', function(d) { return x_1(d.key); })
-        .attr('y', function(d) { return y(d.value); })
-        .attr('width', function(d) { return x_1.bandwidth(); })
-        .attr('height', function(d) { return height - y(d.value); })
-        .style('fill', function(d) { return color(d.key); });
-      // BAR LABELS
-      week.selectAll('text')
-        .data(function(d) { return keys.map(function(key) { return {key: key, value: d.value[key]}; }); })
-          .enter().append('text')
-        .attr('class', 'label')
-        .attr('x', function(d) { return x_1(d.key) + x_1.bandwidth()/2; })
-        .attr('y', function(d) { return y(d.value); })
-        .attr('dy', 15)
-        .style('text-anchor', 'middle')
-        .style('fill', '#fff')
-        .text(function(d) { return format_ticks(d.value); });
-
+      // CHART MARKS
+      draw_bars(nested);
 
       // Axes
-      var axes = g.append('g').attr('class', 'axes');
-      axes.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x_0));
-      axes.append("g")
-          .attr("class", "axis axis--y")
-          .call(d3.axisLeft(y).tickFormat(format_ticks))
-        .append("text")
-          .attr('x', 5)
-          .attr("y", 6)
-          .attr("dy", "0.71em")
-          .attr("fill", "#333")
-          .attr('font-weight', 'bold')
-          .style('text-anchor', 'start')
-          .text("Occurences per Week");
+      draw_axes();
 
-      ////////////////
       // legend
-      var legend = d3.legendColor()
-          .orient('vertical')
-          .title('Keyword')
-          .labels(keys)
-          .shapeWidth(width/20)
-          .shapeHeight(30)
-          .scale(color);
-      g.append('g').attr('class', 'legend').attr('id', 'barLegend')
-        .attr('transform', 'translate(' + (width - margin.right*6) +','+ margin.top +')');
-      g.select('#barLegend').call(legend);
-
-      // pull legend back
-      var leg = g.select('#barLegend').node();
-      var leg_rect = leg.getClientRects()[0];
-      var leg_w = leg_rect.width;
-      var leg_x = width - leg_w;
-      d3.select('#barLegend').attr('transform', 'translate('+leg_x +',' + margin.top + ')');
+      draw_legend();
 
     });
+  }
+
+  function set_scale(nest) {
+    // SCALE SETTING
+    y.domain([0, d3.max(nest.map(function(d) { return d.value; }),
+      function(d) {
+        return d3.max(keys, function(key) { return d[key]; });
+      })
+    ]);
+
+    x_0.domain(date_ticks);
+    x_1.domain(keys).rangeRound([0, x_0.bandwidth()]);
+    color.domain(keys);
+  }
+
+  function draw_bars(nest) {
+    g.selectAll('.weeks').data() ? g.selectAll('.weeks').remove() : null;
+    var week = g.append('g').attr('class', 'weeks').selectAll('.week')
+        .data(nest)
+          .enter().append('g')
+        .attr('class', 'week')
+        .attr('transform', function(d) {
+          return 'translate('+ x_0(d.key) + ',0)'; });
+
+    // RECTS
+    week.selectAll('rect')
+      .data(function(d) { return keys.map(function(key) { return {key: key, value: d.value[key]}; }); })
+        .enter().append('rect')
+      .attr('class', 'bar')
+      .attr('x', function(d) { return x_1(d.key); })
+      .attr('y', function(d) { return y(d.value); })
+      .attr('width', function(d) { return x_1.bandwidth(); })
+      .attr('height', function(d) { return height - y(d.value); })
+      .style('fill', function(d) { return color(d.key); });
+    // BAR LABELS
+    week.selectAll('text')
+      .data(function(d) { return keys.map(function(key) { return {key: key, value: d.value[key]}; }); })
+        .enter().append('text')
+      .attr('class', 'label')
+      .attr('x', function(d) { return x_1(d.key) + x_1.bandwidth()/2; })
+      .attr('y', function(d) { return y(d.value); })
+      .attr('dy', 15)
+      .style('text-anchor', 'middle')
+      .style('fill', '#fff')
+      .text(function(d) { return format_ticks(d.value); });
+  }
+
+  function draw_axes() {
+    g.select('.axes').remove();
+    var axes = g.append('g').attr('class', 'axes');
+    axes.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x_0));
+    axes.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).tickFormat(format_ticks))
+      .append("text")
+        .attr('x', 5)
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("fill", "#333")
+        .attr('font-weight', 'bold')
+        .style('text-anchor', 'start')
+        .text("Occurences per Week");
+  }
+
+  function draw_legend() {
+    d3.select('#barLegend').remove();
+    var legend = d3.legendColor()
+        .orient('vertical')
+        .title('Keyword')
+        .labels(keys)
+        .shapeWidth(width/20)
+        .shapeHeight(30)
+        .scale(color);
+    g.append('g').attr('class', 'legend').attr('id', 'barLegend')
+      .attr('transform', 'translate(' + (width - margin.right*6) +','+ margin.top +')');
+    g.select('#barLegend').call(legend);
+
+    // pull legend back
+    var leg = g.select('#barLegend').node();
+    var leg_rect = leg.getClientRects()[0];
+    var leg_w = leg_rect.width;
+    var leg_x = width - leg_w;
+    d3.select('#barLegend').attr('transform', 'translate('+leg_x +',' + margin.top + ')');
   }
 
   ////////////////////////////////////////////////////
